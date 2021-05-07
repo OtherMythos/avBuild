0e3490c96d69d391bf724790c2344ed1e401a392
edherbert <edward@othermythos.com>
2021-05-07 23:18:29 +0100

    Removed absolute paths.

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+LINUX_BUILD_DIR="${SCRIPT_DIR}/linuxBuild/"
+if [ ! -d ${LINUX_BUILD_DIR} ]; then
+    echo "Could not find linuxBuild scripts in ${LINUX_BUILD_DIR} You probably have a broken repo."
+    exit 1
+fi
 
 if [ -v ${1} ]; then
     echo "Please provide a path of where to build."
@@ -10,12 +17,13 @@ if [ ! -d ${1} ]; then
 fi
 echo "Building to ${1}"
 
-# export UID=$(id -u)
-# export GID=$(id -g)
+export _UID=$(id -u)
+export _GID=$(id -g)
 # docker kill avbuild-container ||
 # docker container rm avbuild-container ||
 docker run --rm \
+    --user ${_UID}:${_GID} \
     -v "${1}:/avbuild/build" \
-    -v "/home/edward/avBuild/linuxBuild:/avbuild/scripts/linuxBuild" \
+    -v "${LINUX_BUILD_DIR}:/avbuild/scripts/linuxBuild" \
     -it avbuild-image \
     /bin/bash
